80a074779c9bbe1df4fee28e6f4b8cb4191b3c4a
Cytron Trainee <support@cytron.com.my>
2021-04-14 12:47:13 +0800

    delete enum

--- custom.ts
+++ custom.ts
@@ -1,19 +1,46 @@
 
 /**
- * Use this file to define custom functions and blocks.
- * Read more at https://makecode.microbit.org/blocks/custom
+ * Board initialization and helper function.
  */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
+//Instruction Set
+const CLEARDISPLAY = 0x01;
+const CURSORSHIFT = 0x10;
 
-/**
- * Custom blocks
- */
+
+//Display Entry Mode
+const ENTRYMODESET = 0x04;
+const ENTRYLEFT = 0x02;
+const ENTRYRIGHT = 0x00;
+const ENTRYSHIFTINCREAMENT = 0x01;
+const ENTRYSHIFTDECREMENT = 0x00;
+
+//Flags for display/cursor shift 
+const DISPLAYMOVE = 0x08;
+const CURSORMOVE = 0X00;
+const MOVERIGHT = 0X04;
+const MOVELEFT = 0X00;
+
+//display control
+const DISPLAYCONTROL = 0X08;
+const DISPLAYON = 0X04;
+const DISPLAYOFF = 0X00;
+const CURSORON = 0X02;
+const CURSOROFF = 0X00;
+const BLINKON = 0X01;
+const BLINKOFF = 0X00;
+const SETCGRAMADDR = 0X40;
+const SETDDRAMADDR = 0X80;
+
+//for functionset
+const FUNCTIONSET = 0X20;
+const _5x10DOTS = 0X04;
+const _5x8DOTS = 0X00;
+const _1LINE = 0X00;
+const _2LINE = 0X08;
+const _8BITMODE = 0X10;
+const  _4BITMODE = 0X00;     
+
 //% weight=100 color=#0fbc11 icon=""
 namespace LCD_i2c {
     /**
